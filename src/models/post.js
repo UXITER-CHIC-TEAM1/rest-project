09353e046d63ef9a5fff13d2565bec041cc9f5b7
thingsub <129747097+thingsub@users.noreply.github.com>
2024-09-24 12:19:23 +0900

    split from config

--- src/models/post.js
+++ src/models/post.js
@@ -1,1 +1,15 @@
+const mongoose = require('mongoose');
 
+// 데이터베이스 스키마 및 모델 추가 (게시글)
+const postSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+    author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    images: [String]
+    // 위치 정보 필드를 제거
+});
+
+const Post = mongoose.model('Post', postSchema);
+
+// 내보내기
+module.exports = Post;
